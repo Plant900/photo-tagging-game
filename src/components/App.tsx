cb45395ee811fd1dd2f2bf6cf99e95d13021b54e
Plant900 <rythodir@gmail.com>
2022-07-22 11:45:55 -0600

    Implement simple map/area instead of square system

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <button onClick={context.signInWithGoogle}>Sign in with Google</button>
       <div>
-        <ImageContainer url="../images/photo-tagging-egor-klyuchnyk1.jpg" />
+        <ImageContainer url="https://cdna.artstation.com/p/assets/images/images/034/427/268/large/egor-klyuchnyk-x-2-seasons-bt.jpg?1612271497" />
       </div>
     </div>
   )
--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -6,55 +6,85 @@ type ImageContainerProps = {
   url: string
 }
 
-type ImageSizeInfo = {
-  width: Number
-  height: Number
+type GuessArea = {
+  x: Number
+  y: Number
 }
 
 export const ImageContainer = ({ url }: ImageContainerProps) => {
-  let imgEl = useRef<HTMLImageElement>(null)
-  let [imageSize, setImageSize] = useState<ImageSizeInfo>()
-  let [numberOfSquares, setNumberOfSquares] = useState<Number | null>(null)
+  let [guessArea, setGuessArea] = useState<GuessArea>({ x: 0, y: 0 })
 
-  let determineSquareCount = (size: ImageSizeInfo | undefined) => {
-    if (size) {
-      let rows = size.width.valueOf() / 40
-      let columns = size.height.valueOf() / 40
-      let number = Math.floor(rows * columns)
-      setNumberOfSquares(number + 10)
-    }
-  }
-
-  let onImageLoad = (info: any) => {
-    let imageSize = { width: info.naturalWidth, height: info.naturalHeight }
-    setImageSize(imageSize)
-    determineSquareCount(imageSize)
-    console.log(imageSize)
-  }
-
-  useEffect(() => {
-    const imgElCurrent = imgEl.current
-
-    if (imgElCurrent) {
-      imgElCurrent.addEventListener('load', () => onImageLoad(imgElCurrent))
-      return () =>
-        imgElCurrent.removeEventListener('load', () =>
-          onImageLoad(imgElCurrent)
-        )
-    }
-  }, [imgEl])
+  // takes coordinates and checks with database?
+  let checkGuess = () => {}
 
   return (
     <div className="image-container">
       <img
-        className="image-1"
-        id="image-1"
-        src={
-          'https://cdna.artstation.com/p/assets/images/images/034/427/268/large/egor-klyuchnyk-x-2-seasons-bt.jpg?1612271497'
-        }
-        ref={imgEl}
+        src={url}
+        onClick={(e) => {
+          let bounds = e.currentTarget.getBoundingClientRect()
+          let x = e.clientX - bounds.left
+          let y = e.clientY - bounds.top
+          setGuessArea({ x, y })
+        }}
+        useMap="#image-map"
       />
-      <Gameboard numberOfSquares={numberOfSquares} />
+      <map name="image-map">
+        <area shape="circle" coords={`${guessArea.x},${guessArea.y}, 30`} />
+      </map>
     </div>
   )
 }
+
+// type ImageSizeInfo = {
+//   width: Number
+//   height: Number
+// }
+
+// export const ImageContainer = ({ url }: ImageContainerProps) => {
+//   let imgEl = useRef<HTMLImageElement>(null)
+//   let [imageSize, setImageSize] = useState<ImageSizeInfo>()
+//   let [numberOfSquares, setNumberOfSquares] = useState<Number | null>(null)
+
+//   let determineSquareCount = (size: ImageSizeInfo | undefined) => {
+//     if (size) {
+//       let rows = size.width.valueOf() / 40
+//       let columns = size.height.valueOf() / 40
+//       let number = Math.floor(rows * columns)
+//       setNumberOfSquares(number + 10)
+//     }
+//   }
+
+//   let onImageLoad = (info: any) => {
+//     let imageSize = { width: info.naturalWidth, height: info.naturalHeight }
+//     setImageSize(imageSize)
+//     determineSquareCount(imageSize)
+//     console.log(imageSize)
+//   }
+
+//   useEffect(() => {
+//     const imgElCurrent = imgEl.current
+
+//     if (imgElCurrent) {
+//       imgElCurrent.addEventListener('load', () => onImageLoad(imgElCurrent))
+//       return () =>
+//         imgElCurrent.removeEventListener('load', () =>
+//           onImageLoad(imgElCurrent)
+//         )
+//     }
+//   }, [imgEl])
+
+//   return (
+//     <div className="image-container">
+//       <img
+//         className="image-1"
+//         id="image-1"
+//         src={
+//           'https://cdna.artstation.com/p/assets/images/images/034/427/268/large/egor-klyuchnyk-x-2-seasons-bt.jpg?1612271497'
+//         }
+//         ref={imgEl}
+//       />
+//       <Gameboard numberOfSquares={numberOfSquares} />
+//     </div>
+//   )
+// }
